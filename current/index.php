22b50667823dfe46f3fb5dddff809536203c79b3
ouhouh23 <dexter32103210@gmail.com>
2023-03-03 17:04:42 +0300

    feat: first database (created, getted, rendered)

--- current/index.php
+++ current/index.php
@@ -1,4 +1,18 @@
 <?php
 
 require "functions.php";
-require "router.php";
+// require "router.php";
+
+$dsn = "mysql:host=localhost;port=3306;dbname=firstbase;charset=utf8mb4";
+
+$pdo = new PDO($dsn, 'root');
+
+$statement = $pdo -> prepare("select * from posts");
+
+$statement -> execute();
+
+$posts = $statement -> fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+	echo "<li>{$post['Title']}</li>";
+}
